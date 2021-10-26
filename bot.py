d36389ec340c0901dfcccf21ac12c10ac77fb154
Minelemon <93196670+Minelemon@users.noreply.github.com>
2021-10-26 22:04:54 +0900

    Initial commit

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+token = "OTAyNTI3MzQ1NTU3MTM5NDg2.YXfuFw.RRDzstfXz2v20KVgw8LxJsPBADM"
+
+@client.event
+async def on_ready():
+
+    print(client.user.name)
+    print('성공적으로 봇이 시작되었습니다.')
+    game = discord.Game('봇이 활동중에 표시될 이름')
+    await client.change_presence(status=discord.Status.onlien, acyivity=game)
+
+
+client.run(token)
---
+++ practice.py
@@ -0,0 +1,5 @@
+# print("맥문이 물여일견\n백견이 불여일타")
+# print("저는 \"나노코딩\"입니다")
+# print("eunhyeokchoi@Eunhyeoks-MacBookAir python test % ")
+
+# print("red apple\r pine")
